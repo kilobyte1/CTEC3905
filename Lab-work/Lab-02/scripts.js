75f7078f0cb83d9621529147075e3bbe6fc60e0f
naage <p2562416@my365.dmu.ac.uk>
2024-02-03 23:35:26 +0000

    updated lab 2

--- Lab-work/Lab-02/scripts.js
+++ Lab-work/Lab-02/scripts.js
@@ -1,3 +1,14 @@
 function greetMe() {
     console.log("Hello!!");
   }
+
+  let myTrigger = document.getElementById("myTrigger");
+
+  let clickCount = 0;
+
+function incrementClickCount() {
+  clickCount++;
+	target.textContent = clickCount;
+}
+
+myTrigger.addEventListener('click', incrementClickCount);
