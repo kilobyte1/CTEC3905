f3bcbce17075dd4de5b182553792f5b59732ea62
naage <p2562416@my365.dmu.ac.uk>
2024-02-14 23:41:30 +0000

    new changes

--- Lab-work/lab-04/script.js
+++ Lab-work/lab-04/script.js
@@ -1,3 +1,4 @@
 menuToggler.addEventListener('click', ev => {
-    menu.classList.toggle('open');
+	menu.classList.toggle('open');
+	menuToggler.textContent = menuToggler.textContent === "×" ? "≡" : "×";
 });
